9e93fd6a3b3f225826f0c55956154c839de6210e
jb0nd87 <jbondoc87@gmail.com>
2020-12-29 19:32:09 -0800

    added routes and cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './components/pages/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import ContactMe from './components/pages/ContactMe';
+import AboutMe from './components/pages/AboutMe';
+import Skills from './components/pages/Skills';
 
 function App() {
 	return (
@@ -11,6 +15,10 @@ function App() {
 				<Navbar />
 				<Switch>
 					<Route path='/' exact component={Home} />
+					<Route path='/projects' component={Projects} />
+					<Route path='/contact-me' component={ContactMe} />
+					<Route path='/skills' component={Skills} />
+					<Route path='/about-me' component={AboutMe} />
 				</Switch>
 			</Router>
 		</>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,7 +10,19 @@ function Cards() {
 				<div className='cards__wrapper'>
 					<ul className='cards__items'>
 						<CardItem
-							src='/public/images/img-9.jpg'
+							src='/src/images/img-9.jpg'
+							text='Project 1'
+							label='Learning'
+							path='/projects'
+						/>
+						<CardItem
+							src='https://res.cloudinary.com/dv01780vo/image/upload/v1600364953/Screen_Shot_2020-09-15_at_4.04.59_PM_vr10ee_qteocd.jpg'
+							text='Project 1'
+							label='Learning'
+							path='/projects'
+						/>
+						<CardItem
+							src='images/img-3.jpg'
 							text='Project 1'
 							label='Learning'
 							path='/projects'
