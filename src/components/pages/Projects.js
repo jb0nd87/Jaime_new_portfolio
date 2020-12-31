b74937137e3d70748c8a018d099354d51c2ceafa
jb0nd87 <jbondoc87@gmail.com>
2020-12-31 15:36:52 -0800

    skills page added

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,9 +5,9 @@ import Cards from '../Cards'
 
 export default function Projects() {
 	return (
-		<>
+		<div>
 			<h1 className='projects'>PROJECTS</h1>
 			<Cards />
-		</>
+		</div>
 	);
 }
--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -2,5 +2,22 @@ import React from 'react';
 import '../../App.css';
 
 export default function Skills() {
-	return <h1 className='skills'>SKILLS</h1>;
+	return (
+		<div>
+			<h1 className='skills'>SKILLS</h1>
+			<div className='skill-icons'>
+				<ul>
+					<li>
+						HTML <i className='fab fa-html5'></i>
+					</li>
+					<li>
+						REACT <i class='fab fa-react'></i>
+					</li>
+					<li>
+						RUBY ON RAILS
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
