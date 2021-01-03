8d6287e4249b96f3d3ffe7b0fedaa249f91e0d11
jb0nd87 <jbondoc87@gmail.com>
2021-01-02 17:35:37 -0800

    added contact form

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -2,5 +2,9 @@ import React from 'react';
 import '../../App.css';
 
 export default function AboutMe() {
-	return <h1 className='about-me'>ABOUT ME</h1>;
+	return (
+		<div>
+			<h1 className='about-me'>ABOUT ME</h1>
+		</div>
+	);
 }
--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import '../../App.css';
+import '../MyForm.css'
+import MyForm from '../MyForm';
 
 export default function ContactMe() {
-	return <h1 className='contact-me'>CONTACT ME</h1>;
+	return (
+		<div className='contact-info'>
+			<h1 className='contact-me'>CONTACT ME</h1>
+			<div>
+				<h1 className='contact'>
+					Contact Info
+				</h1>
+				<p>
+					Email: jbondoc87@gmail.com<br></br>Phone: (510) 672-5306
+				</p>
+			</div>
+			<MyForm />
+		</div>
+	);
 }
